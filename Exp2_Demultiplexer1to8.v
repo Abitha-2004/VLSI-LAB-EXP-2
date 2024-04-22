111882c7147d1d4f274cf2b4d8fbb592989144f9
Abitha-2004 <161303006+Abitha-2004@users.noreply.github.com>
2024-04-22 15:26:48 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,7 @@
-module demux_8(s,d,y);
-input [2:0]s;
-input d;
-output [7:0]y;
-
-
-
-
+module encoder_8_to_3(a0,a1,a2,d0,d1,d2,d3,d4,d5,d6,d7);input d0,d1,d2,d3,d4,d5,d6,d7;
+output a0,a1,a2;
+or g1(a0,d1,d3,d5,d7);
+or g2(a1,d2,d3,d6,d7);
+or g3(a2,d4,d5,d6,d7);
 endmodule
+
